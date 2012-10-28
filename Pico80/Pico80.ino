05dbd3e78759fefe5ec4d9030df99ab4161a0f7e
James Coxon <jacoxon@googlemail.com>
2012-10-28 23:30:26 +0000

    added pico80 - currently transmits 5 strings of RTTY then 10 GFSK strings

---
+++ Pico80/Pico80.ino
@@ -0,0 +1,623 @@
+/*
+* PicoAtlas - James Coxon jacoxon@googlemail.com
+* Experiemental pico payload based upon Arduino Mini Pro, RF22b and a Ublox GPS
+* Designed to be ultra lightweight, current hardware weighs 33g including batteries
+* and can be lifted with a single 92cm foil balloon. Designed to explore long duration
+* floats involving a degree of super-pressure.
+* Transmits RTTY, initially continous then switches to periodic. See http://ukhas.org.uk/projects:picoatlas
+* for more info.
+*
+* Latest code can be found: https://github.com/jamescoxon/PicoAtlas
+*
+* GPS Code from jonsowman and Joey flight computer CUSF
+* https://github.com/cuspaceflight/joey-m/tree/master/firmware
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/  
+#include <SPI.h>
+#include <RF22.h>
+#include <util/crc16.h>
+
+//Setup radio on SPI with NSEL on pin 10
+RF22 rf22(8,0);
+
+//Variables
+int32_t lat = 0, lon = 0, alt = 0;
+uint8_t hour = 0, minute = 0, second = 0, lock = 0, sats = 0;
+unsigned long startGPS = 0;
+int GPSerror = 0, count = 0, n, gpsstatus, lockcount = 0, intTemp = 0, oldLat = 0, total_time = -1, old_total_time = -2, radiostatus = 0, navmode = 9;
+
+int radioPower = A0;
+uint8_t buf[60]; //GPS receive buffer
+char superbuffer [80]; //Telem string buffer
+byte data [80];
+
+// RTTY Functions - from RJHARRISON's AVR Code
+void rtty_txstring (char * string)
+{
+
+	/* Simple function to sent a char at a time to 
+	** rtty_txbyte function. 
+	** NB Each char is one byte (8 Bits)
+	*/
+	char c;
+	c = *string++;
+	while ( c != '\0')
+	{
+		rtty_txbyte (c);
+		c = *string++;
+	}
+}
+
+void rtty_txbyte (char c)
+{
+	/* Simple function to sent each bit of a char to 
+	** rtty_txbit function. 
+	** NB The bits are sent Least Significant Bit first
+	**
+	** All chars should be preceded with a 0 and 
+	** proceded with a 1. 0 = Start bit; 1 = Stop bit
+	**
+	** ASCII_BIT = 7 or 8 for ASCII-7 / ASCII-8
+	*/
+	int i;
+	rtty_txbit (0); // Start bit
+	// Send bits for for char LSB first	
+	for (i=0;i<8;i++)
+	{
+		if (c & 1) rtty_txbit(1); 
+			else rtty_txbit(0);	
+		c = c >> 1;
+	}
+	rtty_txbit (1); // Stop bit
+        rtty_txbit (1); // Stop bit
+}
+
+void rtty_txbit (int bit)
+{
+		if (bit)
+		{
+		  // high
+                  rf22.spiWrite(0x073, 0x03);
+		}
+		else
+		{
+		  // low
+                  rf22.spiWrite(0x073, 0x00);
+		}
+                //delayMicroseconds(9750); // 10000 = 100 BAUD 20150
+                delayMicroseconds(19500); // 10000 = 100 BAUD 20150
+
+}
+
+void setupRadio(){
+  
+  digitalWrite(radioPower, LOW); // Turn on Radio
+  
+  delay(500);
+  
+  rf22.init();
+  
+  rf22.spiWrite(0x71, 0x00); // unmodulated carrier
+  
+  rf22.setFrequency(434.000);
+  
+  rf22.spiWrite(0x6D, 0x04);// turn tx low power 11db
+  
+  rf22.spiWrite(0x07, 0x08); // turn tx on
+  
+  radiostatus = 1;
+  
+}
+
+//***************Hellschreiber******************
+
+struct t_htab { char c; int hellpat[5]; } ;
+
+struct t_htab helltab[] = {
+
+  {'1', { B00000100, B00000100, B01111100, B00000000, B00000000 } },
+  {'2', { B01001000, B01100100, B01010100, B01001100, B01000000 } },
+  {'3', { B01000100, B01000100, B01010100, B01010100, B00111100 } },
+  {'4', { B00011100, B00010000, B00010000, B01111100, B00010000 } },
+  {'5', { B01000000, B01011100, B01010100, B01010100, B00110100 } },
+  {'6', { B00111100, B01010010, B01001010, B01001000, B00110000 } },
+  {'7', { B01000100, B00100100, B00010100, B00001100, B00000100 } },
+  {'8', { B00111100, B01001010, B01001010, B01001010, B00111100 } },
+  {'9', { B00001100, B01001010, B01001010, B00101010, B00111100 } },
+  {'0', { B00111000, B01100100, B01010100, B01001100, B00111000 } },
+  {'A', { B01111000, B00101100, B00100100, B00101100, B01111000 } },
+  {'B', { B01000100, B01111100, B01010100, B01010100, B00101000 } },
+  {'C', { B00111000, B01101100, B01000100, B01000100, B00101000 } },
+  {'D', { B01000100, B01111100, B01000100, B01000100, B00111000 } },
+  {'E', { B01111100, B01010100, B01010100, B01000100, B01000100 } },
+  {'F', { B01111100, B00010100, B00010100, B00000100, B00000100 } },
+  {'G', { B00111000, B01101100, B01000100, B01010100, B00110100 } },
+  {'H', { B01111100, B00010000, B00010000, B00010000, B01111100 } },
+  {'I', { B00000000, B01000100, B01111100, B01000100, B00000000 } },
+  {'J', { B01100000, B01000000, B01000000, B01000000, B01111100 } },
+  {'K', { B01111100, B00010000, B00111000, B00101000, B01000100 } },
+  {'L', { B01111100, B01000000, B01000000, B01000000, B01000000 } },
+  {'M', { B01111100, B00001000, B00010000, B00001000, B01111100 } },
+  {'N', { B01111100, B00000100, B00001000, B00010000, B01111100 } },
+  {'O', { B00111000, B01000100, B01000100, B01000100, B00111000 } },
+  {'P', { B01000100, B01111100, B01010100, B00010100, B00011000 } },
+  {'Q', { B00111000, B01000100, B01100100, B11000100, B10111000 } },
+  {'R', { B01111100, B00010100, B00010100, B00110100, B01011000 } },
+  {'S', { B01011000, B01010100, B01010100, B01010100, B00100100 } },
+  {'T', { B00000100, B00000100, B01111100, B00000100, B00000100 } },
+  {'U', { B01111100, B01000000, B01000000, B01000000, B01111100 } },
+  {'V', { B01111100, B00100000, B00010000, B00001000, B00000100 } },
+  {'W', { B01111100, B01100000, B01111100, B01000000, B01111100 } },
+  {'X', { B01000100, B00101000, B00010000, B00101000, B01000100 } },
+  {'Y', { B00000100, B00001000, B01110000, B00001000, B00000100 } },
+  {'Z', { B01000100, B01100100, B01010100, B01001100, B01100100 } },
+  {'.', { B01000000, B01000000, B00000000, B00000000, B00000000 } },
+  {':', { B00000000, B00100100, B00100100, B00000000, B00000000 } },
+  {',', { B10000000, B10100000, B01100000, B00000000, B00000000 } },
+  {'/', { B01000000, B00100000, B00010000, B00001000, B00000100 } },
+  {'*', { B00000000, B00000000, B00000100, B00001110, B00000100 } }
+
+};
+
+#define N_HELL  (sizeof(helltab)/sizeof(helltab[0]))
+
+void helldelay()
+{
+  //Slow
+  delay(64);
+  delayMicroseconds(450);
+
+}
+
+void on()
+{
+  rf22.spiWrite(0x07, 0x08); //on
+  helldelay();
+  rf22.spiWrite(0x07, 0x01); //off
+}
+
+void hellsend(char c)
+{
+  int i, j, q ;
+  if (c == ' ') {
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+    return ;
+  }
+  for (i=0; i<N_HELL; i++) {
+    if (helltab[i].c == c) {
+      //Serial.print(helltab[i].c) ;
+      
+      for (j=0; j<=4; j++) 
+      {
+        byte mask = B10000000;
+        for (q=0; q<=6; q++)
+        {      
+          if(helltab[i].hellpat[j] & mask) {
+            on();
+          } else {
+            helldelay();
+          }
+          mask >>= 1;
+        }
+      }
+      for (int d=0; d<14; d++){
+        helldelay();  
+      }
+      return ;
+    }
+  }
+  /* if we drop off the end, then we send a space */
+  //Serial.print("?") ;
+}
+
+void hellsendmsg(char *str)
+{
+  while (*str)
+    hellsend(*str++) ;
+  //Serial.println("");
+}
+//************Other Functions*****************
+
+//Function to poll the NAV5 status of a Ublox GPS module (5/6)
+//Sends a UBX command (requires the function sendUBX()) and waits 3 seconds
+// for a reply from the module. It then isolates the byte which contains 
+// the information regarding the NAV5 mode,
+// 0 = Pedestrian mode (default, will not work above 12km)
+// 6 = Airborne 1G (works up to 50km altitude)
+//Adapted by jcoxon from getUBX_ACK() from the example code on UKHAS wiki
+// http://wiki.ukhas.org.uk/guides:falcom_fsa03
+boolean checkNAV(){
+  uint8_t b, bytePos = 0;
+  uint8_t getNAV5[] = { 0xB5, 0x62, 0x06, 0x24, 0x00, 0x00, 0x2A, 0x84 }; //Poll NAV5 status
+  
+  Serial.flush();
+  unsigned long startTime = millis();
+  sendUBX(getNAV5, sizeof(getNAV5)/sizeof(uint8_t));
+  
+  while (1) {
+    // Make sure data is available to read
+    if (Serial.available()) {
+      b = Serial.read();
+      
+      if(bytePos == 8){
+        navmode = b;
+        return true;
+      }
+                        
+      bytePos++;
+    }
+    // Timeout if no valid response in 3 seconds
+    if (millis() - startTime > 3000) {
+      navmode = 0;
+      return false;
+    }
+  }
+}
+
+// Send a byte array of UBX protocol to the GPS
+void sendUBX(uint8_t *MSG, uint8_t len) {
+  for(int i=0; i<len; i++) {
+    Serial.write(MSG[i]);
+  }
+  //Serial.println();
+}
+
+uint16_t gps_CRC16_checksum (char *string)
+{
+	size_t i;
+	uint16_t crc;
+	uint8_t c;
+ 
+	crc = 0xFFFF;
+ 
+	// Calculate checksum ignoring the first two $s
+	for (i = 2; i < strlen(string); i++)
+	{
+		c = string[i];
+		crc = _crc_xmodem_update (crc, c);
+	}
+ 
+	return crc;
+}
+
+void setupGPS() {
+  //Turning off all GPS NMEA strings apart on the uBlox module
+  // Taken from Project Swift (rather than the old way of sending ascii text)
+  uint8_t setNMEAoff[] = {0xB5, 0x62, 0x06, 0x00, 0x14, 0x00, 0x01, 0x00, 0x00, 0x00, 0xD0, 0x08, 0x00, 0x00, 0x80, 0x25, 0x00, 0x00, 0x07, 0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0xA0, 0xA9};
+  sendUBX(setNMEAoff, sizeof(setNMEAoff)/sizeof(uint8_t));
+  
+  delay(500);
+  
+    // Check and set the navigation mode (Airborne, 1G)
+  uint8_t setNav[] = {0xB5, 0x62, 0x06, 0x24, 0x24, 0x00, 0xFF, 0xFF, 0x06, 0x03, 0x00, 0x00, 0x00, 0x00, 0x10, 0x27, 0x00, 0x00, 0x05, 0x00, 0xFA, 0x00, 0xFA, 0x00, 0x64, 0x00, 0x2C, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x16, 0xDC};
+  sendUBX(setNav, sizeof(setNav)/sizeof(uint8_t));
+  
+  delay(500);
+  
+}
+
+void PSMgps(){
+   setupGPS();
+   //set GPS to Eco mode (reduces current by 4mA)
+   uint8_t setEco[] = {0xB5, 0x62, 0x06, 0x11, 0x02, 0x00, 0x00, 0x04, 0x1D, 0x85};
+   sendUBX(setEco, sizeof(setEco)/sizeof(uint8_t));
+   
+   //uint8_t setPSM[] = { 0xB5, 0x62, 0x06, 0x11, 0x02, 0x00, 0x08, 0x01, 0x22, 0x92 }; // Setup for Power Save Mode (Default Cyclic 1s)
+   //sendUBX(setPSM, sizeof(setPSM)/sizeof(uint8_t));
+}
+
+void gpsPower(int i){
+  if(i == 0){
+    //turn off GPS
+    //  uint8_t GPSoff[] = {0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x02, 0x00, 0x00, 0x00, 0x4D, 0x3B};
+    uint8_t GPSoff[] = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0x00, 0x00, 0x08, 0x00, 0x16, 0x74};
+    sendUBX(GPSoff, sizeof(GPSoff)/sizeof(uint8_t));
+    gpsstatus = 0;
+  }
+  else if (i == 1){
+    //turn on GPS
+     //uint8_t GPSon[] = {0xB5, 0x62, 0x02, 0x41, 0x08, 0x00, 0x00, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00, 0x4C, 0x37};
+    uint8_t GPSon[] = {0xB5, 0x62, 0x06, 0x04, 0x04, 0x00, 0x00, 0x00, 0x09, 0x00, 0x17, 0x76};
+    sendUBX(GPSon, sizeof(GPSon)/sizeof(uint8_t));
+    gpsstatus = 1;
+    delay(1000);
+    setupGPS();
+  }
+}
+
+/**
+ * Calculate a UBX checksum using 8-bit Fletcher (RFC1145)
+ */
+void gps_ubx_checksum(uint8_t* data, uint8_t len, uint8_t* cka,
+        uint8_t* ckb)
+{
+    *cka = 0;
+    *ckb = 0;
+    for( uint8_t i = 0; i < len; i++ )
+    {
+        *cka += *data;
+        *ckb += *cka;
+        data++;
+    }
+}
+
+/**
+ * Verify the checksum for the given data and length.
+ */
+bool _gps_verify_checksum(uint8_t* data, uint8_t len)
+{
+    uint8_t a, b;
+    gps_ubx_checksum(data, len, &a, &b);
+    if( a != *(data + len) || b != *(data + len + 1))
+        return false;
+    else
+        return true;
+}
+
+/**
+ * Get data from GPS, times out after 1 second.
+ */
+void gps_get_data()
+{
+    int i = 0;
+    unsigned long startTime = millis();
+    while (1) {
+    // Make sure data is available to read
+    if (Serial.available()) {
+      buf[i] = Serial.read();
+      i++;
+    }
+    // Timeout if no valid response in 1 seconds
+    if (millis() - startTime > 1000) {
+      break;
+    }
+    }
+}
+/**
+ * Check the navigation status to determine the quality of the
+ * fix currently held by the receiver with a NAV-STATUS message.
+ */
+void gps_check_lock()
+{
+    GPSerror = 0;
+    Serial.flush();
+    // Construct the request to the GPS
+    uint8_t request[8] = {0xB5, 0x62, 0x01, 0x06, 0x00, 0x00,
+        0x07, 0x16};
+    sendUBX(request, 8);
+
+    // Get the message back from the GPS
+    gps_get_data();
+    // Verify the sync and header bits
+    if( buf[0] != 0xB5 || buf[1] != 0x62 ) {
+      GPSerror = 11;
+    }
+    if( buf[2] != 0x01 || buf[3] != 0x06 ) {
+      GPSerror = 12;
+    }
+
+    // Check 60 bytes minus SYNC and CHECKSUM (4 bytes)
+    if( !_gps_verify_checksum(&buf[2], 56) ) {
+      GPSerror = 13;
+    }
+    
+    if(GPSerror == 0){
+    // Return the value if GPSfixOK is set in 'flags'
+    if( buf[17] & 0x01 )
+        lock = buf[16];
+    else
+        lock = 0;
+
+    sats = buf[53];
+    }
+    else {
+      lock = 0;
+    }
+}
+
+/**
+ * Poll the GPS for a position message then extract the useful
+ * information from it - POSLLH.
+ */
+void gps_get_position()
+{
+    GPSerror = 0;
+    Serial.flush();
+    // Request a NAV-POSLLH message from the GPS
+    uint8_t request[8] = {0xB5, 0x62, 0x01, 0x02, 0x00, 0x00, 0x03,
+        0x0A};
+    sendUBX(request, 8);
+    
+    // Get the message back from the GPS
+    gps_get_data();
+
+    // Verify the sync and header bits
+    if( buf[0] != 0xB5 || buf[1] != 0x62 )
+        GPSerror = 21;
+    if( buf[2] != 0x01 || buf[3] != 0x02 )
+        GPSerror = 22;
+        
+    if( !_gps_verify_checksum(&buf[2], 32) ) {
+      GPSerror = 23;
+    }
+    
+    if(GPSerror == 0) {
+      // 4 bytes of longitude (1e-7)
+      lon = (int32_t)buf[10] | (int32_t)buf[11] << 8 | 
+          (int32_t)buf[12] << 16 | (int32_t)buf[13] << 24;
+      //lon /= 1000;
+      
+      // 4 bytes of latitude (1e-7)
+      lat = (int32_t)buf[14] | (int32_t)buf[15] << 8 | 
+          (int32_t)buf[16] << 16 | (int32_t)buf[17] << 24;
+      //lat /= 1000;
+      
+      // 4 bytes of altitude above MSL (mm)
+      alt = (int32_t)buf[22] | (int32_t)buf[23] << 8 | 
+          (int32_t)buf[24] << 16 | (int32_t)buf[25] << 24;
+      alt /= 1000;
+    }
+
+}
+
+/**
+ * Get the hour, minute and second from the GPS using the NAV-TIMEUTC
+ * message.
+ */
+void gps_get_time()
+{
+    GPSerror = 0;
+    Serial.flush();
+    // Send a NAV-TIMEUTC message to the receiver
+    uint8_t request[8] = {0xB5, 0x62, 0x01, 0x21, 0x00, 0x00,
+        0x22, 0x67};
+     sendUBX(request, 8);
+
+    // Get the message back from the GPS
+    gps_get_data();
+
+    // Verify the sync and header bits
+    if( buf[0] != 0xB5 || buf[1] != 0x62 )
+        GPSerror = 31;
+    if( buf[2] != 0x01 || buf[3] != 0x21 )
+        GPSerror = 32;
+
+    if( !_gps_verify_checksum(&buf[2], 24) ) {
+      GPSerror = 33;
+    }
+    
+    if(GPSerror == 0) {
+      if(hour > 23 || minute > 59 || second > 59)
+      {
+        GPSerror = 34;
+      }
+      else {
+        hour = buf[22];
+        minute = buf[23];
+        second = buf[24];
+        total_time = hour + minute + second;
+      }
+    }
+}
+
+void prepData() {
+  if(gpsstatus == 1){
+    gps_check_lock();
+    gps_get_position();
+    gps_get_time();
+  }
+  count++;
+  intTemp = temperatureRead( 0x00,0 ) / 2;  //from RFM22
+  intTemp = intTemp - 64;
+  n=sprintf (superbuffer, "$$PICO,%d,%02d:%02d:%02d,%ld,%ld,%ld,%d,%d,%d", count, hour, minute, second, lat, lon, alt, sats, navmode, intTemp);
+  n = sprintf (superbuffer, "%s*%04X\n", superbuffer, gps_CRC16_checksum(superbuffer));
+}
+
+//Taken from RFM22 library + navrac
+uint8_t adcRead(uint8_t adcsel)
+{
+    uint8_t configuration = adcsel;
+    rf22.spiWrite(0x0f, configuration | 0x80);
+    rf22.spiWrite(0x10, 0x00);
+
+    // Conversion time is nominally 305usec
+    // Wait for the DONE bit
+    while (!(rf22.spiRead(0x0f) & 0x80))
+	;
+    // Return the value  
+    return rf22.spiRead(0x11);
+}
+
+uint8_t temperatureRead(uint8_t tsrange, uint8_t tvoffs)
+{
+    rf22.spiWrite(0x12, tsrange | 0x20);
+    rf22.spiWrite(0x13, tvoffs);
+    return adcRead(0x00 | 0x00); 
+}
+
+void re_setup(){
+    //Send commands to GPS
+    gpsPower(1);
+    setupGPS();
+    
+    //Reboot Radio
+    digitalWrite(radioPower, HIGH);
+    radiostatus = 0;
+    delay(1000);
+    setupRadio();
+}
+
+void setup() {
+  pinMode(radioPower, OUTPUT);
+  digitalWrite(A0, LOW); //Turn radio on
+  radiostatus = 0;
+  Serial.begin(9600);
+  delay(500);
+  gpsPower(1); // Turn GPS on
+  //setupRadio();
+  rf22.init();
+  rf22.spiWrite(0x07, 0x08); // turn tx on
+  delay(250);
+  rf22.spiWrite(0x07, 0x01); // turn tx off
+  delay(250);
+}
+
+void CharToByte(char* chars, byte* bytes, unsigned int count){
+    for(unsigned int i = 0; i < count; i++)
+    	bytes[i] = (byte)chars[i];
+}
+
+void loop() {
+  
+  if(count % 100 == 0){
+    re_setup();
+  }
+  
+  if((count % 10) == 0){
+    checkNAV();
+  }
+  
+  prepData();
+
+  rtty_txstring("$$$$$");
+  rtty_txstring(superbuffer);
+  delay(1000);
+
+  if(count % 5 == 0){
+    rf22.reset();
+    rf22.init();
+    rf22.spiWrite(0x6D, 0x04);// turn tx low power 11db
+    rf22.setModemConfig(RF22::GFSK_Rb2Fd5);
+    delay(1000);
+    CharToByte(superbuffer, data, sizeof(superbuffer));
+    //uint8_t data[] = "Hello World!";
+    for (int i=0; i <= 10; i++){
+      rf22.send(data, sizeof(data));
+      rf22.waitPacketSent();
+      delay(1000);
+    }
+    
+    rf22.reset();
+    rf22.spiWrite(0x71, 0x00); // unmodulated carrier
+    rf22.setFrequency(434.000);
+    rf22.spiWrite(0x6D, 0x04);// turn tx low power 11db
+    rf22.spiWrite(0x07, 0x08); // turn tx on
+    
+    delay(10000);
+  }
+ 
+}
